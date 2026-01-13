dd78c795e92f5f31bb1fda9194ffe5df3dc23185
zfarmertic <lilyigbunu@gmail.com>
2026-01-13 19:50:17 +0100

    new file

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,12 @@
-USE alx_book_store;
-SELECT * FROM books;
+-- Query the INFORMATION_SCHEMA to get full details of the 'books' table
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';

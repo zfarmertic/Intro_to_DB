f45c3d420e088439ba9519aa86595fa3e990e356
zfarmertic <lilyigbunu@gmail.com>
2026-01-13 20:11:51 +0100

    another sql added

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,5 @@
 USE alx_book_store;
 
-INSERT INTO Customers (
+INSERT INTO customer (
     customer_id, customer_name, email, address) 
     VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave')

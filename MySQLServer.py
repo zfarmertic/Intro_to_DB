27c815f25558cca87c372094a9e3c27d60691e3e
zfarmertic <lilyigbunu@gmail.com>
2026-01-13 19:16:48 +0100

    data added to mysql server

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,11 +1,19 @@
 import mysql.connector
+try:
+    mydb = mysql.connector.connect(
+        host = 'localhost',
+        user = "root",
+        password = "Igbunu@291997",
+        database = "alx_book_store"
+    )
+    cursor = mydb.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+except mysql.connector.Error as e:
+    print(f"Error: {e}")
 
-mydb = mysql.connector.connect(
-    host = 'localhost',
-    user = "root",
-    password = "Igbunu@291997",
-    database = "alx_book_store"
-)
-cursor = mydb.cursor()
-cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-print("Database 'alx_book_store' created successfully!")
+finally:
+    if mydb in locals() and mydb.is_connected():
+        cursor.close()
+        mydb.close()
+    
